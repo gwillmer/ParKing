e5c723d5b525dbe62f776817bebb1acc5ebb41cb
RodAvila <93148064+RodAvila@users.noreply.github.com>
2023-03-29 16:11:46 -0400

    Updated Project
    fixes some merge conflicts

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-//import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
   loginEmail: string | null = null
   loginPassword: string | null = null
-
   constructor(
     private httpClient: HttpClient,
-    private router: Router) { 
+    private router: Router
+  ){
   }
-
   login(){
     this.httpClient.post('http://localhost:8080/login', {
       email: this.loginEmail,
@@ -31,6 +28,40 @@ export class LoginComponent {
       this.loginPassword = null
     })
   }
+}
+
+/*
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+//import { AuthService } from '../auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginEmail: string | null = null
+  loginPassword: string | null = null
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router) { 
+  }
+  login(){
+    this.httpClient.post('http://localhost:8080/login', {
+      email: this.loginEmail,
+      password: this.loginPassword
+    }).subscribe((response: any) => {
+      if(response){
+        localStorage.setItem('token', response.jwt)
+        this.router.navigate(['profile'])
+      }
+      this.loginEmail = null
+      this.loginPassword = null
+    })
+  }
+}
+*/
 
   /*
   loginUser(event: any) {
@@ -38,11 +69,7 @@ export class LoginComponent {
     const target = event.target
     const username = target.querySelector('#username').value
     const password = target.querySelector('#password').value
-
     //this.Auth.getUserDetails(username, password)
     console.log(username, password)
   }
   */
-
-}
-
