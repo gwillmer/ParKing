7cec6ca18cd2da36dc5318520df2b9b52c0f8e29
sferguson32155 <103959194+sferguson32155@users.noreply.github.com>
2023-03-29 14:29:50 -0400

    Add files via upload

--- app/special-events/special-events.component.ts
+++ app/special-events/special-events.component.ts
@@ -8,3 +8,18 @@ import { Component } from '@angular/core';
 export class SpecialEventsComponent {
 
 }
+export class LoginComponent {
+
+  constructor(/*private Auth: AuthService*/) { }
+
+  loginUser(event: any) {
+    event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+
+    //this.Auth.getUserDetails(username, password)
+    console.log(username, password)
+  }
+
+}
