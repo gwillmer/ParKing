1fff9ae1481e2eaf615f3d29081265a5aabe91d0
chris-krause <84992945+chris-krause@users.noreply.github.com>
2023-03-29 17:58:42 -0400

    Add files via upload

--- cypress/e2e/register.spec.cy.ts
+++ cypress/e2e/register.spec.cy.ts
@@ -1,58 +1,60 @@
 describe('Registration Page Tests', () => {
-  it('Should not register, not an email', () => {
+  it('Should fill every field and clicks button', () => {
     cy.visit('/register')
     cy.url().should('include', 'register')
-    cy.get('#email').type('exampleemail')
-    cy.get('#username').type('username')
-    cy.get('[name="password"]').type('password')
-    cy.get('[name="confirm-password"]').type('password')
+    cy.get('[placeholder="First Name"]').type('First')
+    cy.get('[placeholder="Last Name"]').type('Last')
+    cy.get('[type="email"]').type('example@email.com')
+    cy.get('[type="password"]').first().type('password')
+    cy.get('[type="password"]').eq(1).type('password')
+    cy.get('[placeholder="Phone Number"]').type('0123456789')
     cy.get('[type="submit"]').click()
   });
 
-  it('Should not register, no passwords', () => {
+  it('Should not register if  empty field', () => {
     cy.visit('/register')
-    cy.url().should('include', 'register')
-    cy.get('#email').type('exampleemail@email.com')
-    cy.get('#username').type('username')
-    cy.get('[type="submit"]').click()
+    cy.get('[placeholder="First Name"]').type('First')
+    cy.get('[placeholder="Last Name"]').type('Last')
+    cy.get('[type="email"]').type('example@email.com')
+    cy.get('[type="password"]').first().type('password')
+    cy.get('[type="password"]').eq(1).type('password')
+    cy.get('[type="submit"]').should('be.disabled')
+    cy.get('[placeholder="Phone Number"]').clear()
+    cy.get('[type="submit"]').should('be.disabled')
+    cy.get('[placeholder="Phone Number"]').type('0123456789')
+    cy.get('[type="password"]').first().type('password').clear()
+    cy.get('[type="submit"]').should('be.disabled')
+    cy.get('[type="password"]').first().type('password')
+    cy.get('[type="email"]').clear()
+    cy.get('[type="submit"]').should('be.disabled')
   });
 
-  it('Should not register, no confirmation password', () => {
+  it('Should not submit if email is invalid', () => {
     cy.visit('/register')
-    cy.url().should('include', 'register')
-    cy.get('#email').type('exampleemail@email.com')
-    cy.get('#username').type('username')
-    cy.get('[name="password"]').type('password')
-    cy.get('[type="submit"]').click()
+    cy.get('[placeholder="First Name"]').type('First')
+    cy.get('[placeholder="Last Name"]').type('Last')
+    cy.get('[type="password"]').first().type('password')
+    cy.get('[type="password"]').eq(1).type('password')
+    cy.get('[placeholder="Phone Number"]').type('0123456789')
+    cy.get('[type="email"]').type('exampleemail.com')
+    cy.get('.alert').contains('NOT A VALID EMAIL')
+    cy.get('[type="submit"]').should('be.disabled')
+    cy.get('[type="email"]').clear()
+    cy.get('[type="email"]').type('example@emailcom')
+    cy.get('.alert').contains('NOT A VALID EMAIL')
+    cy.get('[type="submit"]').should('be.disabled')
+    cy.get('[type="email"]').clear()
+    cy.get('[type="email"]').type('example@email.com')
+    cy.get('[type="submit"]').should('be.enabled')
   });
 
-  it('Should not register, no password', () => {
+  it('Should require passwords to match', () => {
     cy.visit('/register')
-    cy.url().should('include', 'register')
-    cy.get('#email').type('exampleemail@email.com')
-    cy.get('#username').type('username')
-    cy.get('[name="password"]').type('password')
-    cy.get('[name="confirm-password"]').type('password')
-    cy.get('[type="submit"]').click()
-  });
-
-  it('Should not register, no email', () => {
-    cy.visit('/register')
-    cy.url().should('include', 'register')
-    cy.get('#username').type('username')
-    cy.get('[name="password"]').type('password')
-    cy.get('[name="confirm-password"]').type('password')
-    cy.get('[type="submit"]').click()
-  });
-
-  it('Should register', () => {
-    cy.visit('/register')
-    cy.url().should('include', 'register')
-    cy.get('#email').type('exampleemail@email.com')
-    cy.get('#username').type('username')
-    cy.get('[name="password"]').type('password')
-    cy.get('[name="confirm-password"]').type('password')
-    cy.get('[type="submit"]').click()
+    cy.get('[type="password"]').first().type('password')
+    cy.get('[type="password"]').eq(1).type('passwor')
+    cy.get('.alert').contains('PASSWORDS DO NOT MATCH')
+    cy.get('[type="password"]').eq(1).type('d')
+    cy.get('.alert').should('not.exist')
   });
 
 })
