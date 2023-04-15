0729628b250d1fce5cc35a58421289b45b4b85b0
RodAvila <93148064+RodAvila@users.noreply.github.com>
2023-04-14 22:45:36 -0400

    Changed input to ngmodel and added listing function to .ts file

--- src/app/special-events/special-events.component.ts
+++ src/app/special-events/special-events.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-special-events',
@@ -6,20 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./special-events.component.css']
 })
 export class SpecialEventsComponent {
-
-}
-export class LoginComponent {
-
-  constructor(/*private Auth: AuthService*/) { }
-
-  loginUser(event: any) {
-    event.preventDefault()
-    const target = event.target
-    const username = target.querySelector('#username').value
-    const password = target.querySelector('#password').value
-
-    //this.Auth.getUserDetails(username, password)
-    console.log(username, password)
+  listingAddress: string | null = null
+  listingZipCode: string | null = null
+  listingStartDate: string | null = null
+  listingEndDate: string | null = null
+  listingDescription: string | null = null
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ){
   }
-
+  listing(){
+    console.log()
+    this.httpClient.post('http://localhost:8080/special-events', {
+      address: this.listingAddress,
+      zipCode: this.listingZipCode,
+      startDate: this.listingStartDate,
+      endDate: this.listingEndDate,
+      description: this.listingDescription,
+    }).subscribe((response: any) => {
+      if(response){
+        localStorage.setItem('token', response.jwt)
+        this.router.navigate(['profile'])
+      }
+      this.listingAddress = null
+      this.listingZipCode = null
+      this.listingStartDate = null
+      this.listingEndDate = null
+      this.listingDescription = null
+    })
+  }
 }
