0f4f2b512641a78f4d5da3e7b8653ab46ce61be0
chris-krause <84992945+chris-krause@users.noreply.github.com>
2023-04-16 23:13:43 -0400

    Add files via upload
    For updated code to work just run "npm install @agm/core" in project root
    If this gives errors, try running "npm i @types/googlemaps@3.39.13 --save-dev" (Angular 11 and the agm core library gave me some issues)

---
+++ buy/buy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy',
+  templateUrl: './buy.component.html',
+  styleUrls: ['./buy.component.css'],
+})
+export class BuyComponent {
+  title = 'My first AGM project';
+  lat = 51.678418;
+  lng = 7.809007;
+}
