2d37fdbccd222cbada9d3694b63559d5d7c18a10
chris-krause <84992945+chris-krause@users.noreply.github.com>
2023-04-19 17:39:04 -0400

    Add files via upload

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-//import { GoogleMapsModule } from '@angular/google-maps'
-
 
 @Component({
   selector: 'app-buy',
   templateUrl: './buy.component.html',
-  styleUrls: ['./buy.component.css']
+  styleUrls: ['./buy.component.css'],
 })
 export class BuyComponent {
   title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+  lat1 =  29.649622559291554;
+  lng1 = -82.33813124232903;
+  lat2 = 25.692014896803297;
+  lng2 = -80.34611209225112;
+  lat3 = 25.690763747181652;
+  lng3 = -80.15951154758172;
+
+  onMouseOver(infoWindow: any, $event: MouseEvent) {
+    infoWindow.open();
+  }
+
+  onMouseOut(infoWindow: any, $event: MouseEvent) {
+    infoWindow.close();
+  }
+
 }
